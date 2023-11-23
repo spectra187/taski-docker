0288473a3005b8c410a39b9e5345a497716293d8
nikitapanyshko <spectra187@yandex.ru>
2023-11-23 19:17:12 +0300

    Add workflow with PEP8 linting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -61,9 +61,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # Добавьте import
-import os
 
-...
 
 # Этими строчками замените текущую настройку DATABASES
 DATABASES = {
